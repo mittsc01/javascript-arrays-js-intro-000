06168f2e9b9637628b9ed2b7a803c9baaf88c267
Scott Mittman <sm2884@nau.edu>
2020-06-11 16:20:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,17 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles" ];
+
+function addElementToBeginningOfArray(arr,element){
+  return [element, ...arr]
+}
+function destructivelyAddElementToBeginningOfArray(arr,element){
+  arr.unshift(element);
+  return arr
+}
+
+function addElementToEndOfArray(arr,element){
+  return [...arr,element]
+}
+function destructivelyAddElementToEndOfArray(arr,element){
+  arr.push(element);
+  return arr
+}
